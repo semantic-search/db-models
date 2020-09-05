49cbdd3d21a856688ea3366bbd4e6d6fd125a084
jainal09 <jainal09gosalia@gmail.com>
2020-09-06 01:43:17 +0530

    working on clean

--- mongo_setup.py
+++ mongo_setup.py
@@ -1,23 +1,23 @@
 import mongoengine
-from . import config
+import globals
 
 
 def global_init():
-    print("checking if env imported" + str(config.DB))
+    print("checking if env imported " + str(globals.DB))
     mongoengine.register_connection(
-        db=config.DB,
-        host=config.MONGO_HOST,
-        port=int(config.PORT),
+        db=globals.DB,
+        host=globals.MONGO_HOST,
+        port=int(globals.PORT),
         alias='core',
-        authentication_source=config.DB,
-        username=config.MONGO_USER,
-        password=config.MONGO_PASSWORD
+        authentication_source=globals.DB,
+        username=globals.MONGO_USER,
+        password=globals.MONGO_PASSWORD
     )
     mongoengine.connect(
-        db=config.DB,
-        host=config.MONGO_HOST,
-        port=int(config.PORT),
-        username=config.MONGO_USER,
-        password=config.MONGO_PASSWORD
+        db=globals.DB,
+        host=globals.MONGO_HOST,
+        port=int(globals.PORT),
+        username=globals.MONGO_USER,
+        password=globals.MONGO_PASSWORD
     )
 
