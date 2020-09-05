a97c67e991759ab0361ef745e69769da0e81131d
jainal09 <jainal09gosalia@gmail.com>
2020-09-05 21:16:44 +0530

    project cleaned up

---
+++ config.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+MONGO_HOST = os.getenv("MONGO_HOST")
+DB = os.getenv('MONGO_DB')
+PORT = os.getenv('MONGO_PORT')
+MONGO_USER = os.getenv('MONGO_USER')
+MONGO_PASSWORD = os.getenv('MONGO_PASSWORD')
